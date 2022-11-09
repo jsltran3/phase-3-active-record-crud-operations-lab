5cd13660f6a0bafaecb5a1cae1bce82f45c306c4
Joseph <jsltran@gmail.com>
2022-11-09 14:50:59 -0800

    commit tree a3d762608c69af2062e78b597d16fd61b87454cf

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,30 @@
 class Movie < ActiveRecord::Base
+    def self.find_all_movies_by_year(year)
+        Movie.where(year: year)
+    end
+
+    def self.create_with_title(title)
+        self.create(title: title)
+    end 
+
+    def self.first_movie
+        self.first
+    end 
+
+    def self.last_movie
+        last 
+    end 
+
+    def self.movie_count
+        Movie.count
+    end
+
+    def self.find_movie_with_id id 
+        Movie.find id 
+    end
+
+    def self.find_movie_with attributes
+        find
+    end
 
 end
